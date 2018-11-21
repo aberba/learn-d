dbf25024210c8a0c994ba7a52a84893060a46ccc
Lawrence Aberba <karabutaworld@gmail.com>
2018-11-21 08:55:35 +0000

    fswatch

---
+++ rx-fswatch/source/app.d
@@ -0,0 +1,45 @@
+import core.thread;
+import core.time;
+import fswatch;
+import rx;
+import std.range : put;
+import std.stdio;
+
+void main()
+{
+    auto watcher = defer!(FileChangeEvent[], (observer) {
+        auto shutdown = false;
+
+        auto thread = new Thread({
+            auto watch = FileWatch("./data", true);
+
+            while (!shutdown)
+            {
+                auto events = watch.getEvents();
+                if (events.length > 0)
+                {
+                    .put(observer, events);
+                }
+
+                Thread.sleep(100.msecs);
+            }
+        });
+        thread.start();
+
+        return new AnonymousDisposable({ shutdown = true; });
+    });
+
+    auto flatten = watcher.map!(events => from(events)).merge();
+    auto changes = flatten.groupBy!(event => event.path).map!(o => o.debounce(2.seconds)).merge();
+
+    // start FileWatch
+    auto disposable = changes.doSubscribe((FileChangeEvent event) {
+        writeln(event);
+    });
+
+    scope (exit)
+        disposable.dispose();
+
+    writeln("Plaese Enter to exit.");
+    readln();
+}
